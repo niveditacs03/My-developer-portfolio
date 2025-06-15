19911d47da93828c9ebde13dd33fb70494d284cd
niveditacs03 <csnivedita2003@gmail.com>
2025-06-15 19:43:40 +0530

    slide

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,10 @@
 import React from "react";
-const Footer=()=>{
-
-    return(
-        <div className="flex justify-center items-center">
-        <div className="flex flex-row justify-center items-center mt-50">
-            <p>Made with 🩵 by Nivedita</p>
-        </div>
-        <a href="./contact">
-        <button type="button" className=" rounded-lg">Let's Connect!</button>
-        </a>
-        </div>
-    )  
-}
-export default Footer
+const Footer = () => {
+  return (
+    <div className="flex flex-row justify-center items-center mt-50">
+      <p>Made with 🩵 by Nivedita</p>
+    </div>
+  );
+};
+export default Footer;
+// add a buton called lets connect
